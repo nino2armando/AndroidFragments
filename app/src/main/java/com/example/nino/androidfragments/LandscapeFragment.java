ed8c593fd486f257a3f7ac94a03e60ead273b93d
Nino <nino2armando@yahoo.com>
2014-08-19 18:40:28 -0700

    basic fragments

---
+++ app/src/main/java/com/example/nino/androidfragments/LandscapeFragment.java
@@ -0,0 +1,22 @@
+package com.example.nino.androidfragments;
+
+import android.annotation.TargetApi;
+import android.app.Fragment;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by nino on 19/08/14.
+ */
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class LandscapeFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_landscape, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/example/nino/androidfragments/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.nino.androidfragments;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.res.Configuration;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Configuration config = getResources().getConfiguration();
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if(config.orientation == Configuration.ORIENTATION_LANDSCAPE){
+            LandscapeFragment landscapeFragment = new LandscapeFragment();
+            fragmentTransaction.replace(android.R.id.content, landscapeFragment);
+        }else{
+            PortraintFragment portraintFragment = new PortraintFragment();
+            fragmentTransaction.replace(android.R.id.content, portraintFragment);
+        }
+        fragmentTransaction.commit();
+        //setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
